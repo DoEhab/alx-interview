7df4df5364a6f16964ffc5ba5255c50cd2ad4596
dohaehab <dohaehab@gmail.com>
2025-05-09 02:10:03 +0300

    task 8

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -8,52 +8,47 @@ This script reads terminal input and print stats
 """
 
 
-def print_stats(total_size, status_codes):
+def print_stats(files_size, all_codes):
     """
     Print stats
-    :param total_size: files size
-    :param status_codes: all status codes
+    :param files_size: files size
+    :param all_codes: all status codes
     :return:
     """
-    print(f"File size: {total_size}")
-    for code in sorted(status_codes.keys()):
-        print(f"{code}: {status_codes[code]}")
+    print(f"File size: {files_size}")
+    for code in sorted(all_codes.keys()):
+        print(f"{code}: {all_codes[code]}")
 
 
-def main():
-    total_size = 0
-    status_codes = defaultdict(int)
-    valid_codes = {'200', '301', '400', '401', '403', '404', '405', '500'}
-    line_count = 0
+total_size = 0
+status_codes = defaultdict(int)
+valid_codes = {'200', '301', '400', '401', '403', '404', '405', '500'}
+line_count = 0
 
-    try:
-        for line in sys.stdin:
-            parsed_input = line.strip().split()
+try:
+    for line in sys.stdin:
+        parsed_input = line.strip().split()
 
-            if len(parsed_input) < 7:
-                continue
+        if len(parsed_input) < 7:
+            continue
 
-            try:
-                ipaddress.ip_address(parsed_input[0])
-                file_size = int(parsed_input[-1])
-                status_code = parsed_input[-2]
-            except ValueError:
-                continue
+        try:
+            ipaddress.ip_address(parsed_input[0])
+            file_size = int(parsed_input[-1])
+            status_code = parsed_input[-2]
+        except ValueError:
+            continue
 
-            if status_code in valid_codes:
-                status_codes[status_code] += 1
-            total_size += file_size
+        if status_code in valid_codes:
+            status_codes[status_code] += 1
+        total_size += file_size
 
-            line_count += 1
-            if line_count % 10 == 0:
-                print_stats(total_size, status_codes)
+        line_count += 1
+        if line_count % 10 == 0:
+            print_stats(total_size, status_codes)
 
-    except KeyboardInterrupt:
-        print_stats(total_size, status_codes)
-        raise
-
+except KeyboardInterrupt:
     print_stats(total_size, status_codes)
+    raise
 
-
-if __name__ == "__main__":
-    main()
+print_stats(total_size, status_codes)
